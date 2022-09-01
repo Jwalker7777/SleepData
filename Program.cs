235dd54be1a99c9bb7a63ba71fed6e4eb9412205
jwalker7777 <jwalker37@my.wctc.edu>
2022-09-01 10:19:53 -0500

    Write data to text file

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace SleepData
 {
@@ -6,7 +7,7 @@ namespace SleepData
     {
         static void Main(string[] args)
         {
-             // ask for input
+            // ask for input
             Console.WriteLine("Enter 1 to create data file.");
             Console.WriteLine("Enter 2 to parse data.");
             Console.WriteLine("Enter anything else to quit.");
@@ -17,20 +18,23 @@ namespace SleepData
             {
                 // create data file
 
-                // ask a question
+                 // ask a question
                 Console.WriteLine("How many weeks of data is needed?");
                 // input the response (convert to int)
                 int weeks = int.Parse(Console.ReadLine());
 
-             // determine start and end date
+                 // determine start and end date
                 DateTime today = DateTime.Now;
                 // we want full weeks sunday - saturday
                 DateTime dataEndDate = today.AddDays(-(int)today.DayOfWeek);
                 // subtract # of weeks from endDate to get startDate
                 DateTime dataDate = dataEndDate.AddDays(-(weeks * 7));
+                
                 // random number generator
                 Random rnd = new Random();
 
+                // create file
+                StreamWriter sw = new StreamWriter("data.txt");
                 // loop for the desired # of weeks
                 while (dataDate < dataEndDate)
                 {
@@ -42,11 +46,12 @@ namespace SleepData
                         hours[i] = rnd.Next(4, 13);
                     }
                     // M/d/yyyy,#|#|#|#|#|#|#
-                    Console.WriteLine($"{dataDate:M/d/yy},{string.Join("|", hours)}");
+                    //Console.WriteLine($"{dataDate:M/d/yy},{string.Join("|", hours)}");
+                    sw.WriteLine($"{dataDate:M/d/yyyy},{string.Join("|", hours)}");
                     // add 1 week to date
                     dataDate = dataDate.AddDays(7);
                 }
-
+                sw.Close();
             }
             else if (resp == "2")
             {
@@ -56,3 +61,6 @@ namespace SleepData
         }
     }
 }
+
+
+
